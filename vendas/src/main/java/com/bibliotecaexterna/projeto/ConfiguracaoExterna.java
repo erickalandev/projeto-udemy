5531491be18a0766d4da0750c12bc42ba058de9f
Erick Alan0123 <erickalan068@gmail.com>
2023-06-08 15:22:29 -0300

    feat(container-ioc): explicacao da anotacao ComponentScan

---
+++ vendas/src/main/java/com/bibliotecaexterna/projeto/ConfiguracaoExterna.java
@@ -0,0 +1,8 @@
+package com.bibliotecaexterna.projeto;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfiguracaoExterna {
+
+}
--- vendas/src/main/java/io/github/erickalandev/VendasApplication.java
+++ vendas/src/main/java/io/github/erickalandev/VendasApplication.java
@@ -4,10 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+/*
+ * @ComponentScan
+ * mais utilizado quando quero usar a aplicacao de outra pessoa, fora da MINHA aplicacao
+ * no caso a minha aplicacao eh o io.github.erickalandev
+ * e o da pessoa com.bibliotecaexterna.projeto
+ */
+@ComponentScan(
+							basePackages = 
+							{
+									"com.bibliotecaexterna.projeto"
+							}
+						)
 @RestController
 public class VendasApplication {
 
