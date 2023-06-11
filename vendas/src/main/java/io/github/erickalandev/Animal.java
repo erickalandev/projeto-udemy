6255f246c19d1141e545723acc456ccdcae4ef2f
Erick Alan0123 <erickalan068@gmail.com>
2023-06-11 15:28:27 -0300

    feat(annotations): criando anotations customizadas

---
+++ vendas/src/main/java/io/github/erickalandev/Animal.java
@@ -0,0 +1,5 @@
+package io.github.erickalandev;
+
+public interface Animal {
+	void fazerBarulho();
+}
--- vendas/src/main/java/io/github/erickalandev/VendasApplication.java
+++ vendas/src/main/java/io/github/erickalandev/VendasApplication.java
@@ -3,8 +3,10 @@ package io.github.erickalandev;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,16 @@ public class VendasApplication {
 
 	@Value("${spring.application.name}")
 	public String applicationName;
+	
+	@Cachorro
+	private Animal animal;
+	
+	@Bean("execute2")
+	public CommandLineRunner execute() {
+		return args -> {
+			this.animal.fazerBarulho();
+		};
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
