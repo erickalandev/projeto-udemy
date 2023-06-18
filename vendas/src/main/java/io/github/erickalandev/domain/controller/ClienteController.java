a8517773c442d8df8ebc3ca761d624add833c7af
Erick Alan0123 <erickalan068@gmail.com>
2023-06-18 14:57:49 -0300

    feat(spring-crud-restful-de-fato): finalizado CRUD do cliente controller

--- vendas/src/main/java/io/github/erickalandev/domain/controller/ClienteController.java
+++ vendas/src/main/java/io/github/erickalandev/domain/controller/ClienteController.java
@@ -1,13 +1,11 @@
 package io.github.erickalandev.domain.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.ExampleMatcher.StringMatcher;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,71 +13,61 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import io.github.erickalandev.domain.entity.Cliente;
 import io.github.erickalandev.domain.repository.Clientes;
 
-@Controller
+@RestController // nao precisa mais usar toda vez a anotation @ResponseBody nas APIs
+@RequestMapping("/api/clientes")
 public class ClienteController {
-	
+
 	private Clientes clientes;
-	
+
 	public ClienteController(Clientes clientes) {
 		this.clientes = clientes;
 	}
-	
-	@GetMapping("/api/clientes/{id}")
-	@ResponseBody
-	public ResponseEntity getClienteById(@PathVariable Integer id) {
-		Optional<Cliente> c = clientes.findById(id);
-		if(c.isPresent()) {
-			return ResponseEntity.ok(c);
-		}
-		return ResponseEntity.notFound().build();
-	}
-	
-	@PostMapping("/api/clientes")
-	@ResponseBody
-	public ResponseEntity<Cliente> save(@RequestBody Cliente cliente) {
-		Cliente clienteSalvo =clientes.save(cliente);
-		return ResponseEntity.ok(clienteSalvo);
-	}
-	
-	@DeleteMapping("/api/clientes/{id}")
-	@ResponseBody
-	public ResponseEntity delete(@PathVariable Integer id) {
-		if(clientes.findById(id).isPresent()) {
-			clientes.deleteById(id);
-			return ResponseEntity.noContent().build();
-		}
-		return ResponseEntity.notFound().build();
-	}
-	
-	@PutMapping("/api/clientes/{id}")
-	@ResponseBody
-	public ResponseEntity put(	@PathVariable Integer id,
-										@RequestBody Cliente cliente) {
+
+	@GetMapping("/{id}")
+	public Cliente getClienteById(@PathVariable Integer id) {
 		return clientes.findById(id)
-				.map(clienteExistente -> {
-					cliente.setId(clienteExistente.getId());
-					clientes.save(cliente);
-					return ResponseEntity.noContent().build();
-				})
-				.orElseGet( () -> ResponseEntity.notFound().build() );
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
 	}
-	
-	@GetMapping("api/clientes")
-	@ResponseBody
-	public ResponseEntity find(Cliente filter) {
-		ExampleMatcher matcher = ExampleMatcher //configuracao de pesquisa
-											.matching() //encontrar clientes
-											.withIgnoreCase() //ignora caixa alto/baixa na pesquisa
-											.withStringMatcher(StringMatcher.CONTAINING); //faz a consultas igual a do like %info%
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente save(@RequestBody Cliente cliente) {
+		return clientes.save(cliente);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Integer id) {
+		clientes.findById(id).map(cliente -> {
+			clientes.delete(cliente);
+			return cliente;
+		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente nao encontrado!"));
+	}
+
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void put(@PathVariable Integer id, @RequestBody Cliente cliente) {
+		clientes.findById(id).map(clienteExistente -> {
+			cliente.setId(clienteExistente.getId());
+			return clientes.save(cliente);
+		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente nao encontrado"));
+	}
+
+	@GetMapping
+	public List<Cliente> find(Cliente filter) {
+		ExampleMatcher matcher = ExampleMatcher // configuracao de pesquisa
+				.matching() // encontrar clientes
+				.withIgnoreCase() // ignora caixa alto/baixa na pesquisa
+				.withStringMatcher(StringMatcher.CONTAINING); // faz a consultas igual a do like %info%
 		Example example = Example.of(filter, matcher);
-		List<Cliente> lista = clientes.findAll(example);
-		
-		return ResponseEntity.ok(lista);
+		return clientes.findAll(example);
 	}
 
 }
