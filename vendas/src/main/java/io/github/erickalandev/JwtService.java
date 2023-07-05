cb784bdd85b5f2e0be1ac4dc1411647d15c1782b
Erick Alan0123 <erickalan068@gmail.com>
2023-07-05 17:07:24 -0300

    feat(security-jwt): gerando token jwt

---
+++ vendas/src/main/java/io/github/erickalandev/JwtService.java
@@ -0,0 +1,60 @@
+package io.github.erickalandev;
+
+import java.util.Date;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Service;
+
+import io.github.erickalandev.domain.entity.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class JwtService {
+	
+	@Value("${security.jwt.expiracao}")
+	private String expiracao;
+	
+	@Value("${security.jwt.chave-assinatura}")
+	private String chaveAssinatura; //chave do servidor
+	
+	public String gerarToken( Usuario usuario ) {
+		long expString = Long.valueOf(expiracao);
+		/*Ao logar, o localDateTime pega o exato momento que entrou(now)
+		 * e soma (plusMinutes) os valor em minutos que foi colocado no parenteses
+		 * no caso 30 minutos(expString) 
+		 */
+		LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(expString);
+		/*Esse instante criado seria a zona, ou seja, a hora vai estar sendo feita de 
+		 * acordo com a zona do Brasil que eh onde estamos(systemDefault)
+		*/
+		Instant instant = dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
+		/*Na api que estamos utilizando a expiracao do jwt tem que ser do tipo Date, por isso a conversao*/
+		Date data = Date.from( instant );
+		return Jwts
+				 		.builder()
+				 		/*o setSubject seria o payload do token, onde colocamos as informacoes do usuario 
+				 		 * por exemplo para ele poder fazer as requisicoes no sistema.*/
+				 		.setSubject(usuario.getLogin())
+				 		.setExpiration(data)
+				 		/*signWith eh a assinatura do token*/
+				 		.signWith( SignatureAlgorithm.HS512, chaveAssinatura )
+				 		/*o compact vai fazer todos esses dados virarem uma string e passar como token*/
+				 		.compact();
+				 		
+	}
+
+	/*metodo de rodar uma aplicacao standarlone*/
+	 public static void main(String[] args) {
+		 ConfigurableApplicationContext context = SpringApplication.run(VendasApplication.class);
+		 JwtService service = context.getBean(JwtService.class);
+		 Usuario usuario = Usuario.builder().login("fulano").build();
+		 String token = service.gerarToken(usuario);
+		 System.out.println(token);
+	 }
+}
