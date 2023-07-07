774b084ec9a6f2b9dcde11ab7ed3d31e008411bb
Erick Alan0123 <erickalan068@gmail.com>
2023-07-07 20:06:27 -0300

    (deploy): criando arquivo war

--- vendas/src/main/java/io/github/erickalandev/VendasApplication.java
+++ vendas/src/main/java/io/github/erickalandev/VendasApplication.java
@@ -2,12 +2,18 @@ package io.github.erickalandev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VendasApplication {
+public class VendasApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
+	
+	/*obs.: o SpringBootServletInitializer do tomcat, faz com que o sistema vire uma
+	 * aplicacao web gerando um war. 
+	 * Deixando de ser uma aplicacao standalone onde geravamos um jar.
+	 * */
 
 }
