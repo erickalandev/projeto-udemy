ac8a28568e0ce764dd5951374bda138a22b8b1b7
Erick Alan0123 <erickalan068@gmail.com>
2023-07-12 11:59:52 -0300

    feat(docker): criando imagem do meu projeto para o docker

---
+++ springdocker/src/test/java/io/github/erickalandev/springdocker/SpringdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.erickalandev.springdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
